3fbb6ad3ddbec5d359daa689aa68e408d6ef2f9d
YuJin Kim <axionbuster@gmail.com>
2024-01-04 08:37:17 -0800

    Minor refactor

--- grav2/Dyn.cpp
+++ grav2/Dyn.cpp
@@ -11,3 +11,20 @@ void Dyn::step()
 void Dyn::bias()
 {
 }
+
+/// <summary>
+/// Compute the acceleration felt by particle at index `i` if it were at
+/// the hypothetical location z.
+/// </summary>
+/// <param name="i">Valid index of the particle</param>
+/// <param name="z">The particle's hypothetical location</param>
+/// <returns>Acceleration, or force divided by the particle's mass</returns>
+C Dyn::accelerate(int i, C const& z) const
+{
+	if (!drv.pair_force) return 0;
+	Entry e(tab[i]); e.z = z;
+	int n = this->n(); C f;
+	for (int j = n - 1; j >= 0; j--)
+		if (i != j) f += drv.pair_force(e, tab[j]);
+	return f / e.m;
+}
--- grav2/Dyn.h
+++ grav2/Dyn.h
@@ -3,56 +3,89 @@
 #include <vector>
 #include <functional>
 
+/// <summary>
+/// Simulation.
+/// </summary>
 class Dyn
 {
 public:
+	/// <summary>
+	/// Simulation parameters in world units.
+	/// </summary>
 	struct Param
 	{
+		/// <summary>
+		/// Universal gravitational constant (units: LLL/T/T/M).
+		/// </summary>
 		double g{};
+		/// <summary>
+		/// Time step (T per step).
+		/// </summary>
 		double dt{};
 	};
 
+	/// <summary>
+	/// Kinematic and dynamic properties.
+	/// </summary>
 	struct Entry
 	{
 		/// <summary>
-		/// Position, velocity, acceleration.
+		/// Position (L), velocity (L/T), acceleration (L/T/T).
 		/// </summary>
 		C z, v, a;
 		/// <summary>
-		/// Mass, radius.
+		/// Mass (M), radius (L).
 		/// </summary>
 		double m{ 1 }, r{ 1 };
 	};
 
+	/// <summary>
+	/// Externally specified behavior.
+	/// </summary>
 	struct Driver
 	{
 		/// <summary>
-		/// Compute the force on the left particle by the right particle.
+		/// Compute the force on the left (first) particle by the right (second) particle.
 		/// </summary>
 		std::function<C(Entry const&, Entry const&)> pair_force;
 	};
 
 	typedef std::vector<Entry> V;
 
-	Dyn(Param const& par, V const& tab)
-		: par(par), tab(tab) {}
-	Dyn(Param const& par, V&& tab)
-		: par(par), tab(std::move(tab)) {}
-	Dyn(Param const& par)
-		: par(par), tab() {}
+	// For these:
+	// I don't copy `drv` (function table) or `copy` (temporary copy of `tab` made at different times).
+	// It also doesn't make sense to copy `par` (parameter array) because it's bitwise copyable.
 
-	/// <summary>
-	/// Exposed set of drivers.
-	/// </summary>
-	Driver driver;
+	Dyn(Param const& par) : par(par) {}
+	Dyn(Dyn const& dyn) : par(dyn.par), tab(dyn.tab), drv(dyn.drv), copy() {}
+	Dyn(Dyn&& dyn) noexcept : par(dyn.par), tab(std::move(dyn.tab)), drv(dyn.drv), copy() {}
+	Dyn& operator=(Dyn const& dyn) noexcept
+	{
+		if (&dyn == this) return *this;
+		par = dyn.par, tab = dyn.tab, drv = dyn.drv;
+		return *this;
+	}
+	Dyn& operator=(Dyn&& dyn) noexcept
+	{
+		if (&dyn == this) return *this;
+		par = dyn.par, drv = dyn.drv;
+		tab = std::move(dyn.tab);
+		return *this;
+	}
+
 	/// <summary>
 	/// Exposed simulation parameters.
 	/// </summary>
 	Param par;
 	/// <summary>
-	/// Exposed dynamical table.
+	/// Exposed dynamical table. (Stores all kinematical and dynamical variables
+	/// of all particles). Do not exceed the size of `int` (signed).
 	/// </summary>
 	V tab;
+	/// <summary>
+	/// Exposed set of drivers.
+	/// </summary>
+	Driver drv;
 
 	/// <summary>
 	/// Precompute all accelerations before the first iteration.
@@ -75,7 +108,7 @@ public:
 	/// Count the number of particles.
 	/// </summary>
 	/// <returns></returns>
-	int n() const { return tab.size(); }
+	int n() const { return (int)tab.size(); }
 
 private:
 	/// <summary>
@@ -91,18 +124,6 @@ private:
 	/// <param name="i">Valid index of the particle</param>
 	/// <param name="z">The particle's hypothetical location</param>
 	/// <returns>Acceleration, or force divided by the particle's mass</returns>
-	C accelerate(int i, C const& z) const
-	{
-		if (!driver.pair_force) throw std::exception("pair_force() must exist");
-		Entry e(tab[i]); e.z = z;
-		C f;
-		int n = this->n();
-		for (int j = n - 1; j >= 0; j--)
-		{
-			if (i == j) continue;
-			f += driver.pair_force(e, tab[j]);
-		}
-		return f / e.m;
-	}
+	C accelerate(int i, C const& z) const;
 };
 
