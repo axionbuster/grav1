12cf363020a0339f1b80c95170673a0a82fc336f
YuJin Kim <axionbuster@gmail.com>
2024-01-04 19:41:05 -0800

    grav2: Finally, implement clumping
    I'm crying it's working LOLOLOL (cry)

--- grav2/Geo2.h
+++ grav2/Geo2.h
@@ -14,7 +14,6 @@
 struct Halton
 {
 	unsigned n{ 0 }, d{ 1 }, x{}, y{}, b{};
-public:
 	/// <summary>
 	/// Initialize a sequence with the given prime base.
 	/// </summary>
@@ -25,48 +24,105 @@ public:
 	/// </summary>
 	/// <returns>A number in the open interval (0, 1).</returns>
 	double next();
+private:
+	friend struct Halton2D;
+	/// <summary>
+	/// Create a default instance in an unspecified (erroneous) state.
+	/// </summary>
+	Halton() : b(0) {}
 };
 
 /// <summary>
 /// Quasi-Monte Carlo integrator in two dimensions
 /// using Halton low-discrepancy sequences for fast convergence.
 /// 
-/// Region of integration: Unit square in (0, 1) x (0, 1).
+/// Region of integration: Unit square in (0,1) x (0,1).
 /// </summary>
-struct HaltonQuadrature2D
+struct Halton2D
 {
 	/// <summary>
 	/// Exposed internal state (Halton low-discrepancy sequences).
 	/// </summary>
 	Halton h[2];
+
 	/// <summary>
-	/// Compute the frequency of the points that pass the test.
-	/// 
-	/// Points are generated using the internal array of sequences (`haltons`),
-	/// whose states will change.
+	/// Create a valid instance of Halton2D.
 	/// </summary>
-	/// <param name="test">A test against a point in x:(0, 1) by y:(0, 1), in this order,
-	/// expressed as a complex number (real is x, imag is y).</param>
-	/// <param name="n">Positive sample count.</param>
-	/// <returns>Frequency of `true` values returned by the test in the closed interval [0, n].</returns>
-	int count(std::function<bool(C const &)> const& test, int n = 100);
+	Halton2D() { h[0] = Halton(2), h[1] = Halton(3); }
+
+	/// <summary>
+	/// Generate a point in the (0,1) x (0,1) square.
+	/// </summary>
+	C next() { return C(h[0].next(), h[1].next()); }
 };
 
 /// <summary>
-/// Compute the area of the crescent in the right disk (circle 1)
-/// formed by the intersection between that and the left disk (circle 0),
-/// if such an intersection exists; otherwise, 0.
+/// Store a pair of possibly intersecting circles in a reoriented coordinate system,
+/// where the left circle is centered at the origin, and the right circle
+/// has a center on the positive real axis. Here lengths do not change;
+/// only orientation does.
 /// </summary>
-/// <param name="q">Halton sequence bundle.</param>
-/// <param name="c0">Center of the left circle.</param>
-/// <param name="r0">Positive radius of the left circle.</param>
-/// <param name="c1">Center of the right circle.</param>
-/// <param name="r1">Positive radius of the right circle.</param>
-/// <param name="n">Positive number of trials for the quasi-Monte Carlo quadrature.</param>
-/// <returns>The approximate area, or 0.</returns>
-double area_right_crescent(
-	HaltonQuadrature2D &q,
-	C c0, double r0,
-	C c1, double r1,
-	int n = 100
-);
+struct CircularIntersection
+{
+public:
+	/// <summary>
+	/// Construct data about the (possible) intersection between two circles.
+	/// </summary>
+	/// <param name="c0">Center of the left circle.</param>
+	/// <param name="r0">Radius of the left circle.</param>
+	/// <param name="c1">Center of the right circle.</param>
+	/// <param name="r1">Radius of the right circle.</param>
+	CircularIntersection(C c0, double r0, C c1, double r1)
+		: lrr(r0* r0), rrr(r1* r1)
+	{
+		using std::min;
+		using std::max;
+		c1 -= c0;
+		c = abs(c1);
+		double _l = min(-1., c - r1), _r = max(1., c + r1);
+		double _b = min(-1., -r1), _t = max(1., r1);
+		m = (_l + _r) / 2;
+		d = max(_r - _l, _t - _b);
+	}
+
+	/// <summary>
+	/// Decide whether the point (reoriented coord.) is in the left circle.
+	/// </summary>
+	bool left(C const& p) const { return std::norm(p) < lrr; }
+
+	/// <summary>
+	/// Decide whether the point (reoriented coord.) is in the right circle.
+	/// </summary>
+	bool right(C const& p) const { return std::norm(p - c) < rrr; }
+
+	/// <summary>
+	/// Perform a trial, calling the user-defined process `f` that takes in
+	/// the point `p` which is in the reoriented coordinate system.
+	/// </summary>
+	/// <param name="h">A point in the (0,1) x (0,1) square</param>
+	/// <param name="f">A process for which p is in the reoriented coordinate system</param>
+	void monte(C const& h, std::function<void(C const& p)> const& f) const { f(from01(h)); }
+
+	/// <summary>
+	/// Transform a point in the (0,1) x (0,1) square to the bounding square.
+	/// </summary>
+	C from01(C const& h) const { return (h - C(.5, .5)) * d + m; }
+
+	/// <summary>
+	/// Recall the area of the bounding square.
+	/// </summary>
+	double bounding_area() const { return d * d; }
+
+	/// <summary>
+	/// Recall the x-coordinate of the center of the bounding square.
+	/// </summary>
+	double bounding_midpoint_x() const { return m; }
+private:
+	/// <summary>
+	/// Center of the right circle;
+	/// Dimension (side length) of the bounding square;
+	/// Midpoint of the bounding square;
+	/// Squared radius of the left circle (and right circle).
+	/// </summary>
+	double c{}, d{}, m{}, lrr{}, rrr{};
+};
--- grav2/Source.cpp
+++ grav2/Source.cpp
@@ -29,7 +29,12 @@ static void draw_particle(Dyn const& dyn, int i)
 /// <summary>
 /// Universal gravitational constant (units: LLL/T/T/M).
 /// </summary>
-constexpr double G = 0.05;
+constexpr double G = 0.15;
+
+/// <summary>
+/// Decide whether both components of the complex number are finite.
+/// </summary>
+static bool finite(C const& c) { return isfinite(c.real()) && isfinite(c.imag()); }
 
 /// <summary>
 /// Force on the left particle (l) due to the right particle (r).
@@ -39,9 +44,60 @@ constexpr double G = 0.05;
 /// <returns>Force (units: ML/T/T/T)</returns>
 C newton_gravity(Dyn::Entry const& l, Dyn::Entry const& r)
 {
+	// Low-discrepancy sequences needed for parts of the calculation.
+	static Halton2D hh;
+
 	C s = r.z - l.z; double as = abs(s);
-	C f = G * l.m * r.m * (1 / as / as / as) * s;
-	return isfinite(f.real()) && isfinite(f.imag()) ? f : 0;
+
+	if (as < l.r + r.r)
+	{
+		// The circles representing them intersect.
+		// The simple calculation below doesn't apply.
+		// So, integrate the infinitesimal forces to get the total force for each
+		// small patch of the region of the left circle that is outside
+		// the right circle.
+
+		// Number of Monte-Carlo trials.
+		constexpr int M = 100;
+		// Force per mass (integrated).
+		C fpm;
+		// Number of pieces sampled in the left crescent (one-sided lune---just "lune").
+		int n{};
+		// Circular intersection.
+		CircularIntersection sect(l.z, l.r, r.z, r.r);
+		// Computation of lunar area and force.
+		auto compute = [&](C const& p)
+			{
+				// `p` is sampled from a certain square in
+				// a reoriented coordinate system, where
+				// the left circle is centered at the origin, and
+				// the right circle as at (`as`, 0). Lengths have
+				// not changed.
+
+				if (!sect.left(p) || sect.right(p)) return;
+				n++;
+				C arm = as - p; double dist = abs(arm);
+				// [***] `fpm` will be missing the factors of: M (=# trials), G, dm.
+				fpm += 1 / dist / dist / dist / M * arm;
+			};
+		// Boom.
+		for (int i = M - 1; i >= 0; i--)
+			sect.monte(hh.next(), compute);
+		// If no sample hit, the integration failed.
+		if (!n) return 0;
+		// Compute dm by the ratio -> dm : m = 1 : n
+		// (where dm: infinitesimal mass, m: mass of left particle,
+		// n: number of samples hit).
+		double dm = l.m / n;
+		// [***] Multiply back the missing factors in `fpm`.
+		fpm *= M * G * dm;
+		return finite(fpm) ? fpm : 0;
+	}
+	else
+	{
+		C f = G * l.m * r.m * (1 / as / as / as) * s;
+		return finite(f) ? f : 0;
+	}
 }
 
 int wWinMain(void* _0, void* _1, void* _2, int _3)
@@ -52,13 +108,13 @@ int wWinMain(void* _0, void* _1, void* _2, int _3)
 	{
 		// Generate this many (n) particles.
 		int constexpr n = 100;
-		// auto seed = []() { std::random_device dev; return dev(); }();
-		auto rng = std::mt19937(1234);
-		C const rot = std::polar(1., PI64 / 6);
+		auto seed = []() { std::random_device dev; return dev(); }();
+		auto rng = std::mt19937(seed);
+		C const rot = std::polar(0., PI64 / 6);
 		for (int i = n - 1; i >= 0; i--)
 		{
 			typedef std::uniform_real_distribution<> D;
-			D z(-100., 100.), v(-0.1, 0.1), m(1., 5.), r(1., 5.);
+			D z(-40, 40.), v(0, 0), m(1., 5.), r(1., 5.);
 			auto sca = [&](D& d) { return d(rng); };
 			auto vec = [&](D& d) { return C(sca(d), sca(d)); };
 			Dyn::Entry e;
@@ -74,10 +130,10 @@ int wWinMain(void* _0, void* _1, void* _2, int _3)
 	dyn.drv.pair_force = newton_gravity;
 
 	// Rendering
-	float constexpr px_per_l = 1.f;
+	float constexpr px_per_l = 2.f;
 
 	// Raylib.
-	InitWindow(600, 600, "a");
+	InitWindow(600, 600, "Gravity");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose())
